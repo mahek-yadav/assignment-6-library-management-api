c5a0580260a89485844635d7829a37a8464d105c
Mahek yadav <maheky2306@icloud.com>
2026-09-02 12:02:18 +0530

    Add Mahek Yadav Library Management API

---
+++ MahekYadav-150096725142/src/config/firebase.js
@@ -0,0 +1,19 @@
+const { initializeApp, cert, getApps } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const path = require("path");
+
+const serviceAccount = require(
+    path.join(__dirname, "../../serviceAccountKey.json")
+);
+
+if (getApps().length === 0) {
+    initializeApp({
+        credential: cert(serviceAccount)
+    });
+}
+
+const db = getFirestore();
+
+module.exports = {
+    db
+};
